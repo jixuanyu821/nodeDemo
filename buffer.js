df7ef07542cbc94f7bb33bf73bac52ae7555e09a
jixuanyu <jixy@hzcominfo.com>
2019-12-09 15:00:47 +0800

    bufferdemo

--- buffer.js
+++ buffer.js
@@ -27,15 +27,27 @@ const buf6 = Buffer.from('test','latin1')
 console.log(buf6.toString('hex'),'buf6')
 
 // buf.write(string[, offset[, length]][, encoding])  buffer写入缓冲区
-const buf7 = Buffer.alloc(256);
+const buf7 = Buffer.alloc(2);
 var len = buf7.write('www.baidu.com');
-console.log('写入字节：'+ len);
+console.log('写入字节：'+ len+','+buf7.toString('utf8'));
 
 const buf8 = Buffer.alloc(26);
 for(var i = 0;i<26;i++){
   buf8[i] = i+97
 }
+/**
+ * @description: Buffer.toString 读取缓冲区数据
+ * @param {encoding }  编码方式，默认utf-8
+ * @param {start} 开始位置
+ * @param {end} 结束为止
+ * @return: 读取的数据
+ */
 console.log(buf8.toString('ascii'))
 console.log(buf8.toString('ascii',0,5))
 console.log(buf8.toString('utf8',0,8))
 console.log(buf8.toString(undefined,0,5))
+
+const buf9 = Buffer.from([1,2,3,4]);
+const json = JSON.stringify(buf9)
+console.log(buf9)
+
